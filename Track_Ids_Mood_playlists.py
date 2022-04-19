861c170d215d32507c71fda6f95eddc016b345cd
BLoTz33 <81542514+BLoTz33@users.noreply.github.com>
2022-04-19 06:21:10 -0700

    Update Track_Ids_Mood_playlists.py
    created csv files

--- Track_Ids_Mood_playlists.py
+++ Track_Ids_Mood_playlists.py
@@ -1,10 +1,11 @@
+#import required functions
 import requests
 import base64
-
-client_id = 'xxxxxxxxxxx'
-client_secret = 'xxxxxxxxxxxxx'
+import pandas as pd
+from create_datafram_tracks import Dataframe_Audio_features
+client_id = 'xxxxxx'
+client_secret = 'xxxxx'
 auth_url = 'https://accounts.spotify.com/api/token'
-
 base_url = 'https://api.spotify.com/v1/playlist/'
 
 Grant_token = {'grant_type': 'client_credentials'}
@@ -28,14 +29,14 @@ headers = {
     'Authorization':f'Bearer {access_token}',
 }
 
-list_of_playlist_ids = {'Happy':'5DnBWoczKx4mgk1pkSdrtu',
+dict_of_playlist_ids = {'Happy':'5DnBWoczKx4mgk1pkSdrtu',
                         'Relaxed':'0B1cW8x7Mopg6Du5BJ4spM',
                         'Energized':'6HU206rbi8DmcaKh5LUnOe',
                         'Angry':'3aBeWOxyVcFupF8sKMm2k7',
                         'Sad':'4rFp8l9vekheKOpeJLVkar',
                         }
 
-list_of_track_ids = {'Happy':set(),
+dict_of_track_ids = {'Happy':set(),
                         'Relaxed':set(),
                         'Energized':set(),
                         'Angry':set(),
@@ -43,26 +44,27 @@ list_of_track_ids = {'Happy':set(),
                      }
 
 
-for key in list_of_playlist_ids:
-    playlist_id = list_of_playlist_ids[key]
+
+for key in dict_of_playlist_ids:
+    playlist_id = dict_of_playlist_ids[key]
     request_url = f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks'
     pr = requests.get(request_url,headers=headers)
     if pr.status_code:
         playlist_data = pr.json().get('items')
         for tr in playlist_data:
             track_id = tr.get('track').get('id')
-            list_of_track_ids[key].add(track_id)
+            dict_of_track_ids[key].add(track_id)
 
 track_retrive_header = {
     'Content-Type':'application/json',
     'Authorization':f'Bearer {access_token}',
 }
 
-list_for_track_features = [[],[],[],[],[]]
+list_for_track_features = [[] for _ in range(5)]
 
-for key in list_of_track_ids:
-    Id_list = list_of_track_ids[key]
-    j = 0
+j = 0
+for key in dict_of_track_ids:
+    Id_list = dict_of_track_ids[key]
     for i in Id_list:
         request_url = f'https://api.spotify.com/v1/audio-features/{i}'
         pr = requests.get(request_url, headers=track_retrive_header)
@@ -70,6 +72,26 @@ for key in list_of_track_ids:
             track_features = pr.json()
             track_data_copy = track_features.copy()
             list_for_track_features[j].append(track_data_copy)
+            print('yes', i)
+
         else:
-            print('Failed')
+            print('Audio Features failed to aquire!')
+            break
     j += 1
+    if pr.status_code not in range(200,299):
+        print('function did not complete')
+        break
+#Create Names
+Files_Names_csv = ['Happy.csv','Relaxed.csv','Energized.csv','Angry.csv','Sad.csv']
+#formate to dataframes
+data1 = Dataframe_Audio_features(list_for_track_features[0])
+data2 = Dataframe_Audio_features(list_for_track_features[1])
+data3 = Dataframe_Audio_features(list_for_track_features[2])
+data4 = Dataframe_Audio_features(list_for_track_features[3])
+data5 = Dataframe_Audio_features(list_for_track_features[4])
+#covert to csv for future use
+data1.to_csv(f'{Files_Names_csv[0]}')
+data2.to_csv(f'{Files_Names_csv[1]}')
+data3.to_csv(f'{Files_Names_csv[2]}')
+data4.to_csv(f'{Files_Names_csv[3]}')
+data5.to_csv(f'{Files_Names_csv[4]}')
